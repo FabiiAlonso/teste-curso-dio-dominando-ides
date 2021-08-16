c4e5fb2b370c37448a83ab3f24002f4ec3fffe25
Fabiana Alonso <fabiana.badiglian@gmail.com>
2021-08-16 10:15:44 -0300

    create class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 /* ctrl+shift+/ comenta o bloco selecionado
@@ -10,8 +12,56 @@ public class PrimeiroPrograma {
         System.out.println("Hello World! " + (a+b));
 */
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O misterio dos tres gatos",1000);
+        System.out.println(livro1);
     }
 
+    static class Livro{
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Livro livro = (Livro) o;
+            return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nome, numPaginas);
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+    }
 }
